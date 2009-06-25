6a9b0b5d2b4c5105a0ad312662d3daf7c09c5fdb
Fabrice Luraine <fabrice.luraine@sofa-design.net>
2009-06-25 17:13:23 +0200

    Adding a task in TODO

--- views/posts/index.html.php
+++ views/posts/index.html.php
@@ -2,7 +2,7 @@
   <? if(empty($posts)): ?>
   <p>No posts</p>
   <? else: ?>
-    <? 
+    <?
       foreach($posts as $post)
       {
         echo render('posts/_post.html.php', null, array('post'=>$post));
