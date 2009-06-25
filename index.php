1cf7d803ebe5a5a5f32dfafe3abf50711605a21d
Fabrice Luraine <fabrice.luraine@sofa-design.net>
2009-06-25 12:28:56 +0200

    First version of the main app file: defining basic logic structure

---
+++ index.php
@@ -0,0 +1,128 @@
+<?php
+
+# Setting global options of our application
+function configure()
+{
+  
+}
+
+
+# will be executed bfore each controller function
+function before()
+{
+  html_layout('layouts/default.html.php');
+}
+
+# Defining routes and controllers
+# ----------------------------------------------------------------------------
+# RESTFul map:
+#
+# GET    /posts             blog_posts_index
+# GET    /posts/:id         blog_posts_show 
+# GET    /posts/new         blog_posts_new 
+# POST   /posts             blog_posts_create
+# GET    /posts/:id/edit    blog_posts_edit 
+# PUT    /posts/:id         blog_posts_update
+# DELETE /posts/:id         blog_posts_destroy
+# GET    /                  blog_posts_home (redirect to /posts)
+
+# matches GET /
+dispatch('/', 'blog_posts_home');
+  function blog_posts_home()
+  {
+    redirect(url_for('posts'));
+  }
+
+# matches GET /posts  
+dispatch('/posts', 'blog_posts_index')
+  function blog_posts_index()
+  {
+    $posts = posts_find_all();
+    set('posts', $posts);
+    html('posts/index.html.php');
+  }
+  
+# matches GET /posts/1  
+dispatch('/posts/:id', 'blog_posts_show')
+  function blog_posts_show()
+  { 
+    if( $post = posts_find(params('id')) )
+    {
+      set('post', $post);
+      html('posts/show.html.php');
+    }
+    else
+    {
+      halt(NOT_FOUND, "This post doesn't exists");
+    }
+    
+  }
+  
+# matches GET /posts/new  
+dispatch('/posts/new', 'blog_posts_new')
+  function blog_posts_new()
+  { 
+    html('posts/new.html.php');
+  }
+  
+# matches POST /posts
+dispatch_post('/posts', 'blog_posts_create')
+  function blog_posts_create()
+  { 
+    if($post_id = posts_create($_POSTS))
+    {
+      redirect(url_for('posts', $post_id));
+    }
+    else
+    {
+      halt(SERVER_ERROR, "AN error occurd while trying to create a new post")
+    }
+  }
+  
+# matches GET /posts/1/edit  
+dispatch('/posts/:id/edit', 'blog_posts_edit')
+  function blog_posts_edit()
+  {
+    if($post = posts_find(params('id')))
+    {
+      html('posts/edit.html.php');
+    }
+    else
+    {
+      halt(NOT_FOUND, "This post doesn't exists. Can't edit it.");
+    }
+  }
+  
+# matches PUT /posts/1
+dispatch_put('/posts/:id', 'blog_posts_update')
+  function blog_posts_update()
+  {
+    $post_id = params('id');
+    if(posts_update($post_id, $_POSTS))
+    {
+      redirect(url_for('posts', $post_id));
+    }
+    else
+    {
+      halt(SERVER_ERROR, "An error occured while trying to update post ".$post_id);
+    }
+  }
+  
+# matches DELETE /posts/1
+dispatch_delete('/posts/:id', 'blog_posts_destroy')
+  function blog_posts_destroy()
+  {
+    $post_id = params('id');
+    if($post = posts_destroy($post_id))
+    {
+      redirect(url_for('posts'));
+    }
+    else
+    {
+      halt(SERVER_ERROR, "An error occured while trying to destroy post ".$post_id);
+    }
+  }
+
+
+?>
+
