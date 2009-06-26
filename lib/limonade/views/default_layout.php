13d139713b1ae4d00c9c497d7e41b53d0a0089f0
Fabrice Luraine <fabrice.luraine@sofa-design.net>
2009-06-26 18:27:30 +0200

    a

---
+++ lib/limonade/views/default_layout.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Limonade, the fizzy PHP micro-framework</title>
+</head>
+<body>
+<?=$content;?>
+</body>
+</html>
