6105b631ebbc596ec8697a4b5fbb4278aaa4bd93
Fabrice Luraine <fabrice.luraine@sofa-design.net>
2009-06-26 18:55:48 +0200

    Place the delete link in the show view and remove it from the edit view

--- views/posts/_post.html.php
+++ views/posts/_post.html.php
@@ -5,6 +5,8 @@
     <?=h($post['body'])?>
   </div>
   <p>
-    <a href="<?= url_for('posts', $post['id'], 'edit'); ?>">edit</a> 
+    <a href="<?= url_for('posts', $post['id']); ?>">show</a> | 
+    <a href="<?= url_for('posts', $post['id'], 'edit'); ?>">edit</a> |
+    <a href="<?= url_for('posts', $post['id']);?>" onclick="if (confirm('Are you sure?')) { var f = document.createElement('form'); f.style.display = 'none'; this.parentNode.appendChild(f); f.method = 'POST'; f.action = this.href;var m = document.createElement('input'); m.setAttribute('type', 'hidden'); m.setAttribute('name', '_method'); m.setAttribute('value', 'DELETE'); f.appendChild(m); f.submit(); };return false;">delete</a>
   </p>
 </div>
--- views/posts/edit.html.php
+++ views/posts/edit.html.php
@@ -1,7 +1,3 @@
 <h2>Edit post</h2>
 
 <?= render('posts/_form.html.php', null, array('post' => $post)); ?>
-
-<p>
-  <a href="<?=url_for('posts', $post['id']);?>" onclick="if (confirm('Are you sure?')) { var f = document.createElement('form'); f.style.display = 'none'; this.parentNode.appendChild(f); f.method = 'POST'; f.action = this.href;var m = document.createElement('input'); m.setAttribute('type', 'hidden'); m.setAttribute('name', '_method'); m.setAttribute('value', 'DELETE'); f.appendChild(m); f.submit(); };return false;">Delete this post</a>
-</p>
