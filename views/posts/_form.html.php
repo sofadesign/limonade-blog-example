f0741e1384b9dc7868f2b4743633a3ce6c71d2d8
Fabrice Luraine <fabrice.luraine@sofa-design.net>
2009-06-26 18:51:32 +0200

    Fixing many bugs and enhance views

--- views/posts/_form.html.php
+++ views/posts/_form.html.php
@@ -1,20 +1,26 @@
 
 <? 
-  $action = empty($post['id']) ? url_for('posts') : $url_for('posts', $post['id']);
-  $method = empty($post['id']) ? 'POST' : 'PUT'; 
+  $action = empty($post['id']) ? url_for('posts') : url_for('posts', $post['id']);
+  $method = empty($post['id']) ? 'POST' : 'PUT';
 ?>
-<form action="<?=$action?>" method="<?=$method?>">
+<form action="<?=$action?>" method="POST">
   <fieldset id="post_form">
     <legend>Post</legend>
+    <? if(!empty($post['id'])): ?>
+    <input type="hidden" name="_method" value="PUT" id="_method">
+    <? endif; ?>
     <p>
       <label for="post_title">Title</label>
       <input type="text" name="post[title]" value="<?=h($post['title']);?>" id="post_title">
     </p>
     <p>
       <label for="post_body">Body</label>
-      <textarea name="post['body']" id="post_body"rows="8" cols="40"><?=h($post['body']);?></textarea>
+      <textarea name="post[body]" id="post_body"rows="8" cols="40"><?=h($post['body']);?></textarea>
     </p>
     
-    <p><input type="submit" value="<?= empty($post['id']) ? "Create" : "Update" ?> &rarr;"></p>
+    <p>
+      <input type="submit" value="<?= empty($post['id']) ? "Create" : "Update" ?> &rarr;">
+      or <a href="<?=$action?>">cancel</a>
+    </p>
   </fieldset>
 </form>
--- views/posts/_post.html.php
+++ views/posts/_post.html.php
@@ -2,9 +2,9 @@
   <h2><?=h($post['title'])?></h2>
   <p class="date">Modified at: <?=strftime('%c', strtotime($post['modified_at']))?></p>
   <div class="content">
-    <?=h($post['title'])?>
+    <?=h($post['body'])?>
   </div>
   <p>
-    <a href="<?= url_for('posts', $post['id']); ?>"></a>
+    <a href="<?= url_for('posts', $post['id'], 'edit'); ?>">edit</a> 
   </p>
 </div>
