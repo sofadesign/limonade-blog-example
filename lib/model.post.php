22da6628569ff333326d1432a7d6a76cb9bd5f39
Fabrice Luraine <fabrice.luraine@sofa-design.net>
2009-06-26 18:30:05 +0200

    Add localtime option in INSERT sql

--- lib/model.post.php
+++ lib/model.post.php
@@ -55,7 +55,7 @@ function post_create($data)
 	$db = option('db_conn');
 	$sql = <<<SQL
 	INSERT INTO `posts` ("title", "body", "created_at", "modified_at") 
-	VALUES (:title, :body, DATETIME('NOW'), DATETIME('NOW', 'localtime'))
+	VALUES (:title, :body, DATETIME('NOW', 'localtime'), DATETIME('NOW', 'localtime'))
 SQL;
 	$stmt = $db->prepare($sql);
 	$stmt->bindValue(':title', $data['title'], PDO::PARAM_STR);
