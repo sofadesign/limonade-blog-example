e866a7e5b282fb8abfbbf7cf53c49027d3a524f6
Fabrice Luraine <fabrice.luraine@sofa-design.net>
2009-06-26 19:40:50 +0200

    Update README file

--- index.php
+++ index.php
@@ -1,7 +1,43 @@
 <?php
 # ############################################################################ #
 #                                                                              #
-#  LIMONADE BLOG EXAMPLE                                                       #
+#  # LIMONADE BLOG EXAMPLE #                                                   #
+#                                                                              #
+#  ---                                                                         #
+#                                                                              #
+#  by [Fabrice Luraine](http://github.com/sofadesign/) and                     #
+#  [Mathias Standaert](http://github.com/organicweb/)                          #
+#                                                                              #
+#  This basic blog application will show you how to use                        #
+#  the [Limonade PHP](http://www.sofa-design.net/limonade) micro-framework     #
+#                                                                              #
+#  This code is for learning purpose and is not intended to be used "as is"    #
+#  in a production environement.                                               #
+#                                                                              #
+#  ---                                                                         #
+#                                                                              #
+#  Copyright (c) 2009 Fabrice Luraine, Mathias Standaert                       #
+#                                                                              #
+#  Permission is hereby granted, free of charge, to any person                 #
+#  obtaining a copy of this software and associated documentation              #
+#  files (the "Software"), to deal in the Software without                     #
+#  restriction, including without limitation the rights to use,                #
+#  copy, modify, merge, publish, distribute, sublicense, and/or sell           #
+#  copies of the Software, and to permit persons to whom the                   #
+#  Software is furnished to do so, subject to the following                    #
+#  conditions:                                                                 #
+#                                                                              #
+#  The above copyright notice and this permission notice shall be              #
+#  included in all copies or substantial portions of the Software.             #
+#                                                                              #
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,             #
+#  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES             #
+#  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                    #
+#  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT                 #
+#  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,                #
+#  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING                #
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR               #
+#  OTHER DEALINGS IN THE SOFTWARE.                                             #
 #                                                                              #
 # ############################################################################ #
 
