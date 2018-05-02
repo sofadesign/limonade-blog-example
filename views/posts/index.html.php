dcccbe17d1f60604e502d4f67dc19eff9e17410f
signalwerk <sh@signalwerk.ch>
2018-05-02 20:45:16 +0200

    ADD: full php mention

--- views/posts/index.html.php
+++ views/posts/index.html.php
@@ -1,12 +1,12 @@
 <div id="posts">
-  <? if(empty($posts)): ?>
+  <?php if(empty($posts)): ?>
   <p>No posts</p>
-  <? else: ?>
+  <?php else: ?>
     <?
       foreach($posts as $post)
       {
         echo render('posts/_post.html.php', null, array('post'=>$post));
       }
     ?>
-  <? endif; ?>
+  <?php endif; ?>
 </div>
